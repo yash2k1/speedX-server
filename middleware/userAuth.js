85b798fce64201fc93ed4417ae9ba0e78c8097e8
yash gupta <yashgupta1mole@gmail.com>
2023-09-20 04:13:53 +0530

    final

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -5,12 +5,21 @@ if(bearer==undefined){
     return res.status(401).send("no token");
 }
 const token=bearer.split(" ")[1];
-console.log(token);
+// console.log(token);
 if(!token){
     return res.status(401).send("user is not authorized");
 }
-console.log(token)
-const validate=jwt.verify(token,process.env.secreatKey);
+// console.log(token)
+const validate=jwt.verify(token,process.env.secreatKey,(err,res)=>{
+    if(err){
+        console.log(err.message)
+        return `token expired`;
+    }
+    return res;
+});
+if(validate==="token expired"){
+   return res.send({err:"token expired"});
+}
 if(validate){
     return next();
 }
